585501c5c6b8d509d4fe0a0f1bab123654b94233
Caimanbrujo <186770762+CaimanBrujo@users.noreply.github.com>
2025-06-13 23:36:47 +0200

    feat(restaurant-page): implemented tab switching logic in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 import loadHome from './home';
+import loadMenu from './menu';
+import loadContact from './contact';
+
+function clearContent() {
+    const content = document.querySelector('#content');
+    content.innerHTML = '';
+}
+
+function setTabListeners() {
+    const homeBtn = document.querySelector('#home-btn');
+    const menuBtn = document.querySelector('#menu-btn');
+    const contactBtn = document.querySelector('#contact-btn');
+
+    homeBtn.addEventListener('click', () => {
+        clearContent();
+        loadHome();
+    });
+
+    menuBtn.addEventListener('click', () => {
+        clearContent();
+        loadMenu();
+    });
+
+    contactBtn.addEventListener('click', () => {
+        clearContent();
+        loadContact();
+    });
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     loadHome();
+    setTabListeners();
 });
